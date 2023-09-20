5b7341865d6804bb8d072b25b5bbd7550ad876a4
akanshavrma <134785932+akansha2222@users.noreply.github.com>
2023-09-20 17:33:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 function getFormvalue() {
     //Write your code here
-	let fnme = document.getElementById("fname")
-let lnme = document.getElementById("lname")
+	let fnme = document.getElementById("fname");
+let lnme = document.getElementById("lname");
 	alert(`${fnme} ${lnme}`)
 
 }
